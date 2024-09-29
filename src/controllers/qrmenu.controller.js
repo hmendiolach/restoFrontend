040cfff6b9243d1913d3c7ccecaa9293ac8d80ce
dhruvilxcode <dhruvilxcode@gmail.com>
2024-09-29 11:41:54 +0530

    update qr menu for cors.

--- src/controllers/qrmenu.controller.js
+++ src/controllers/qrmenu.controller.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { API } from "../config/config";
 
 export async function getQRMenuInit(qrcode) {
-    axios.defaults.withCredentials = true;
+    // axios.defaults.withCredentials = true;
     try {
         const response = await axios.get(`${API}/qrmenu/${qrcode}`);
         return response;
